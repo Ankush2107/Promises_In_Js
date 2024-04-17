7e43094842365993a9071a0ad76bfadace442a9d
Ankush2107 <ankush.kumar210701@gmail.com>
2024-04-17 09:33:38 +0530

    using promises to complete the task

--- demo.js
+++ demo.js
@@ -1,8 +1,25 @@
+// let x = 10
+// let y = 20
+
+// setTimeout(() => {
+//     y = 100
+// }, 2000)
+
+// console.log(x + y);
+
+
+// Now using promises to do this task
 let x = 10
 let y = 20
 
-setTimeout(() => {
-    y = 100
-}, 2000)
+let firstPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(100)
+    }, 2000)
+})
 
-console.log(x + y);
+.then((data) => {
+    let b = data
+
+    console.log(x + b)
+})
